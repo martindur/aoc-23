d48b0496c9cf0b2bc8d89e562e9206524403e1ae
Martin Durhuus <martindurhuus@gmail.com>
2023-12-03 13:06:27 +0100

    finished day 2

--- day2.exs
+++ day2.exs
@@ -4,6 +4,15 @@ defmodule Day2 do
   # First part matches any numbers 20+, and second part matches above the limits and color
   @regex ~r/[1-9]\d{2,}|[2-9]\d|1[3-9] red|1[4-9] green|1[5-9] blue/
 
+  defp get_max_color(values) do
+    values
+      |> List.flatten
+      |> map(&String.replace(&1, ~r/[^\d]/, ""))
+      |> reject(&(&1 == ""))
+      |> map(&String.to_integer/1)
+      |> max
+  end
+
   def puzzle1 do
     {:ok, body} = File.read("day02_input.txt")
 
@@ -11,7 +20,7 @@ defmodule Day2 do
     |> reject(&(&1 == ""))
     |> map(&String.split(&1, ":"))
     |> map(&List.pop_at(&1, 0))
-    |> filter(fn { game_id, [rounds] } -> 
+    |> filter(fn { _, [rounds] } -> 
       [] == Regex.scan(@regex, rounds)
     end)
     |> map(fn { "Game " <> game_id, _ } ->
@@ -25,9 +34,26 @@ defmodule Day2 do
     {:ok, body} = File.read("day02_input.txt")
 
     String.split(body, "\n")
+    |> reject(&(&1 == ""))
+    |> map(&String.split(&1, ":"))
+    |> map(&List.pop_at(&1, 0))
+    |> map(fn { _, [rounds] } -> 
+      %{
+        "greens" => Regex.scan(~r/\d{1,} green/, rounds),
+        "blues" => Regex.scan(~r/\d{1,} blue/, rounds),
+        "reds" => Regex.scan(~r/\d{1,} red/, rounds)
+      }
+    |> IO.inspect
+    end)
+    |> map(fn %{ "greens" => g, "blues" => b, "reds" => r} ->
+      get_max_color(g) * get_max_color(b) * get_max_color(r)
+    end)
+    |> IO.inspect
+    |> sum
     |> IO.inspect
   end
 
 end
 
-Day2.puzzle1
+#Day2.puzzle1
+Day2.puzzle2
